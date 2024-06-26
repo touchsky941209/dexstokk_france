86da617bfcbc60bde71d54d023d34245267ae053
touchsky941209 <dannyelia941209@gmail.com>
2024-06-26 09:08:15 +0900

    edit

--- src/component/myoffer/MyOffer.tsx
+++ src/component/myoffer/MyOffer.tsx
@@ -127,6 +127,15 @@ export default function StickyHeadTable(props: any) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [sellerAddress, setSellerAddress] = React.useState([])
+  const [isDialogOpen, setDialogOpen] = React.useState(false)
+  const [updateOfferId, setUpdateOfferId] = React.useState<any>()
+  const [previousOfferPrice, setPreviousOfferPrice] = React.useState<any>()
+  const [previousOfferAmount, setPreviousOfferAmount] = React.useState<any>()
+  const [updateOfferPrice, setUpdateOfferPrice] = React.useState<any>()
+  const [updateOfferAmount, setUpdateOfferAmount] = React.useState<any>()
+  const close = () => {
+    setDialogOpen(false)
+  };
   const gotoShowOffer = (index: any) => {
     navigate('/showoffer', { state: { index } });
   }
@@ -161,14 +170,12 @@ export default function StickyHeadTable(props: any) {
           const _availableQuantity: any = Number(item.amount) / Math.pow(10, 18)
 
           const offer = await estokkYamContract.methods.showOffer(_offerId).call()
-
           if (item.seller === account || item.buyer === account) {
             if (isSearchFilter(_offerTokenAddres, _buyerTokenAddres, props.searchType, tokens)) {
               offerArray.push(createData(_offerId, item.offerToken, item.buyerToken, _officialYield, _offerYield, _yeidlDelta, _officialprice, _priceToken, _priceDelta, _availableQuantity))
               offerSellerAddress.push(offer[2])
             }
           }
-
         })
       )
 
@@ -181,10 +188,6 @@ export default function StickyHeadTable(props: any) {
 
   }, [props])
 
-  React.useEffect(() => {
-    console.log("Seller Address => ", sellerAddress)
-  }, [sellerAddress])
-
   const OfferEdit = async (offerId: any) => {
     try {
       const offer = await estokkYamContract.methods.showOffer(offerId).call()
@@ -193,6 +196,12 @@ export default function StickyHeadTable(props: any) {
         toastr.info("You are not offer creater.")
         return
       }
+
+      setPreviousOfferPrice(offer[4])
+      setPreviousOfferAmount(offer[5])
+
+      setUpdateOfferId(offerId)
+      setDialogOpen(true)
 
     } catch (err) {
 
@@ -211,77 +220,145 @@ export default function StickyHeadTable(props: any) {
     } catch (err) {
     }
   }
+
+  const setUpdateOffer = async () => {
+    try {
+      const result = await estokkYamContract.methods.updateOffer(updateOfferId, updateOfferPrice, updateOfferAmount).send({ from: account })
+      toastr.success("Offer is updated successfully")
+    } catch (err) {
+      toastr.error("Offer update is failed")
+    }
+  }
+
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440, minHeight: 300 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.offerid}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align} onClick={() => {
-                          gotoShowOffer(row.offerid)
-                        }}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                    <TableCell>
-                      <button className='w-[20px]'
-                        onClick={() => { OfferEdit(row.offerid) }}
-                      >
-                        <img src='./img/contractEdit.png' width="42" height="42" alt='contractEdit'></img>
-                      </button>
-                    </TableCell>
+    <>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 440, minHeight: 300 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.offerid}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align} onClick={() => {
+                            gotoShowOffer(row.offerid)
+                          }}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                      <TableCell>
+                        <button className='w-[20px]'
+                          onClick={() => { OfferEdit(row.offerid) }}
+                        >
+                          <img src='./img/contractEdit.png' width="42" height="42" alt='contractEdit'></img>
+                        </button>
+                      </TableCell>
 
-                    <TableCell>
-                      {/* sellerAddress[index] === account && */}
-                      <button className='w-[20px]'
-                        onClick={() => { OfferDelete(row.offerid) }}
-                      >
-                        <img src='./img/contractDelete.png' width="42" height="42" alt='contractEdit'></img>
-                      </button>
+                      <TableCell>
+                        {/* sellerAddress[index] === account && */}
+                        <button className='w-[20px]'
+                          onClick={() => { OfferDelete(row.offerid) }}
+                        >
+                          <img src='./img/contractDelete.png' width="42" height="42" alt='contractEdit'></img>
+                        </button>
 
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper >
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper >
+
+      <Dialog
+        open={isDialogOpen}
+        onClose={close}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <div className='md:w-[600px] w-[80vw] h-[600px] p-4'>
+          <p className="w-[100%] text-[33px] font-bold text-center">Update</p>
+          <p className='text-[22px]'>Selected Offer</p>
+
+          <p className='text-[19px] font-bold'>  OfferID </p>
+          <p className='text-[18px]'>{updateOfferId}</p>
+
+          <p className='text-[19px] font-bold'>Offer Price</p>
+          <p className='text-[18px]'>{String(previousOfferPrice)}</p>
+
+          <p className='text-[19px] font-bold'>Offer Amount</p>
+          <p className='text-[18px]'>{String(Number(previousOfferAmount) / Math.pow(10, 18))}</p>
+
+          <p className='mt-3 font-bold'>Price</p>
+          <input className='w-[100%] h-9 border-[2px] mt-1 border-[#00b3ba] rounded-md focus:outline-none'
+            onChange={(e) => {
+              setUpdateOfferPrice(e.target.value)
+            }}
+          >
+          </input>
+
+          <p className='mt-3 font-bold'>Amount</p>
+          <input className='w-[100%] h-9 border-[2px] mt-1 border-[#00b3ba] rounded-md focus:outline-none'
+            onChange={(e) => {
+              setUpdateOfferAmount(Number(e.target.value) * Math.pow(10, 18))
+            }}
+          >
+
+          </input>
+
+          <div className='flex justify-between item-center w-[100%] h-[50px] mt-5'>
+            <button className='w-[48%] bg-red-400 rounded-md text-[20px] text-[white]'
+              onClick={() => {
+                setUpdateOffer()
+              }}
+            >
+              Update
+            </button>
+
+            <button className='w-[48%] bg-orange-400 rounded-md text-[20px] text-[white]'
+              onClick={() => {
+                setDialogOpen(false)
+                setUpdateOfferAmount(0)
+                setUpdateOfferPrice(0)
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Dialog>
+    </>
   );
 }
-
-
 
 
